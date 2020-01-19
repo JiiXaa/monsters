f16e28cbe197f8f33f598103dd777d241b96686f
unknown <anythingfx@gmail.com>
2020-01-19 12:04:37 +0000

    adding files for gh pages

---
+++ .history/src/App_20200113145950.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import './App.css';
+import { CardList } from './components/card-list.component';
+
+class App extends Component {
+  state = {
+    monsters: []
+  };
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <input type='search' placeholder='search monsters' />
+        <CardList monsters={this.state.monsters} />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ .history/src/App_20200113151244.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import './App.css';
+import { CardList } from './components/card-list.component';
+
+class App extends Component {
+  state = {
+    monsters: [],
+    searchField: ''
+  };
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <input
+          type='search'
+          placeholder='search monsters'
+          onChange={e => {
+            this.setState({ searchField: e.target.value }, () =>
+              console.log(this.state)
+            );
+          }}
+        />
+        <CardList monsters={this.state.monsters} />
+      </div>
+    );
+  }
+}
+
+export default App;
