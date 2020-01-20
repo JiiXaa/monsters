a29314916c22a78fefae3948eb85f96c60e7ff86
unknown <anythingfx@gmail.com>
2020-01-20 12:01:03 +0000

    Updates

---
+++ precache-manifest.5172be2ed911e33a12671e676689f747.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9e8a3323c7b74ff3fb69224ec95853d",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "06b818756dbc343e2f5a",
+    "url": "/monsters/static/css/main.8a23b1cc.chunk.css"
+  },
+  {
+    "revision": "3d106ff954b3b583647e",
+    "url": "/monsters/static/js/2.e66b9b07.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters/static/js/2.e66b9b07.chunk.js.LICENSE"
+  },
+  {
+    "revision": "06b818756dbc343e2f5a",
+    "url": "/monsters/static/js/main.07d4d281.chunk.js"
+  },
+  {
+    "revision": "27edc721a646a18481ad",
+    "url": "/monsters/static/js/runtime-main.9b332845.js"
+  }
+]);
